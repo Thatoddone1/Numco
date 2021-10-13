5b7b48b2cc0f612a7c58984e50c46701f4f02b43
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2021-10-13 11:48:55 -0400

    WIP RSA implemintation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import rsa
+from Crypto.PublicKey import RSA
 import pyperclip as pc
+from Crypto.Cipher import PKCS1_OAEP
 encoded = ""
 decoded = ""
 
@@ -7,26 +8,29 @@ decoded = ""
 
 while True:
     encOrDec = input("Do you want to encode or decode (e/d) ")
-    encoded = ''
-    decoded = ''
     if encOrDec == "e":
         text = input("what do you want to encode? ")
-        text = text.encode("utf8")
-        pubkey = input("whats your pubkey? ")   
-        encode = rsa.encrypt(text, pubkey)
-        print(pubkey, privkey)
-        pc.copy(encoded)
-        print(encoded)
-
-
+        encryptor = PKCS1_OAEP.new(pubkey)
+        encrypted = encryptor.encrypt(text)
+        pc.copy(encrypted)
+        print(encrypted)
     elif encOrDec == "d":
         text = input("what do you want to decode? ")
-        privkey = input("whats your privkey? ")
-        decoded = rsa.decrypt(text, privkey)
-        decoded = decoded.decode("utf8")
+        decryptor = PKCS1_OAEP.new(privkey)
+        decoded= decryptor.decrypt(text)
         print(decoded)
     elif encOrDec == "g":
-        (pubkey, privkey) = rsa.newkeys(2048)
-        print (pubkey)
-        print("")
-        print(privkey)
+        key = RSA.generate(2048)
+        pubkey = key.publickey()
+        pubKeyPEM = pubkey.exportKey()
+        print(pubKeyPEM)
+        f = open('mykey.pem','wb')
+        f.write(key.export_key('PEM'))
+        f.close()
+    elif encOrDec == "i":
+        f = open('mykey.pem','r')
+        privkey = f.read()
+        f.close()
+    elif encOrDec == "c":
+        outerpubkey = input("What is the recivers public key")
+        pubkey = outerpubkey
