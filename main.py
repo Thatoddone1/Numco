799c08372807e28c86a43554a9ae458d2d30fdd1
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2021-10-14 17:59:45 -0400

    even more of a WIP

--- main.py
+++ main.py
@@ -3,62 +3,20 @@ import os
 import pygubu
 import tkinter as tk
 import tkinter.ttk as ttk
-encrypted = ""
+encoded = ""
+decoded = ""
+def decode(text):
+    decoded = ""
+    for letter in text:
+        decoded = decoded + inout[[f_inout[1] for f_inout in inout].index(letter)][0]
+        return decoded
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-PROJECT_UI = os.path.join(PROJECT_PATH, "newproject")
-
-class Numco:
-    def __init__(self, master=None):
-        # build ui
-        self.frame2 = ttk.Frame(master)
-        self.labelframe1 = ttk.Labelframe(self.frame2)
-        self.entry1 = ttk.Entry(self.labelframe1)
-        _text_ = '''Message Here'''
-        self.entry1.delete('0', 'end')
-        self.entry1.insert('0', _text_)
-        self.entry1.pack(padx='100', pady='10', side='top')
-        self.button2 = ttk.Button(self.labelframe1)
-        self.button2.configure(text='Encode')
-        self.button2.pack(pady='10', side='top')
-        self.labelframe1.configure(height='200', text='Numco', width='200')
-        self.labelframe1.pack(side='top')
-        self.separator1 = ttk.Separator(self.frame2)
-        self.separator1.configure(orient='horizontal', style='Toolbutton')
-        self.separator1.pack(side='top')
-        self.entry2 = ttk.Entry(self.frame2)
-        _text_ = '''Encoded Here'''
-        self.entry2.delete('0', 'end')
-        self.entry2.insert('0', _text_)
-        self.entry2.pack(pady='10', side='top')
-        self.button3 = ttk.Button(self.frame2)
-        self.button3.configure(text='Decode')
-        self.button3.pack(pady='10', side='top')
-        self.message2 = tk.Message(self.frame2)
-        self.encrypted = tk.StringVar(value='')
-        self.message2.configure(textvariable = encrypted)
-        self.message2.pack(pady='50', side='top')
-        self.frame2.configure(height='2000', width='2000')
-        self.frame2.pack(side='top')
-
-        # Main widget
-        self.mainwindow = self.frame2
-    
-
-    def run(self):
-        self.mainwindow.mainloop()
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = Numco(root)
-    app.run()
-
-
-
-
-
-
+def encode(text):
+    encoded = ""
+    for letter in text:
+        encoded = encoded + inout[[f_inout[0] for f_inout in inout].index(letter)][1]
+    pc.copy(encoded)
+    return encoded
 
 
 inout = [
@@ -130,35 +88,120 @@ inout = [
         ["B", "M"],
         ["N", "<"],
         ["M", ">"],
+        ["_", "~"],
 
                 ]
 
-while True:
-    encOrDec = input("Do you want to encode or decode (e/d/exit) ")
-    encoded = ''
-    decoded = ''
-    if encOrDec == "e":
-        text = input("what do you want to encode? ")
 
-        for letter in text:
-            encoded = encoded + inout[[f_inout[0] for f_inout in inout].index(letter)][1]
-        pc.copy(encoded)
-        print(encoded)
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_UI = os.path.join(PROJECT_PATH, "newproject")
 
 
-    elif encOrDec == "d":
-        text = input("what do you want to decode? ")
+conorgui = input("Would you like the console UI or the GUI UI(gui/con) ")
+if conorgui == "gui":
 
-        for letter in text:
-            decoded = decoded + inout[[f_inout[1] for f_inout in inout].index(letter)][0]
-        print(decoded)
+
+
+    class NumcoGuiApp:
+        def __init__(self, master=None):
+            # build ui
+            self.frame1 = ttk.Frame(master)
+            self.labelframe1 = ttk.Labelframe(self.frame1)
+            self.Encode = ttk.Label(self.labelframe1)
+            self.Encode.configure(text='Encode')
+            self.Encode.pack(side='top')
+            self.entry1 = ttk.Entry(self.labelframe1)
+            self.encodetext = tk.StringVar(value='Text here')
+            self.encodetext = tk.StringVar(value= "Text here")
+            self.entry1.configure(textvariable=self.encodetext)
+            _text_ = '''Text here'''
+            self.entry1.delete('0', 'end')
+            self.entry1.insert('0', _text_)
+            self.entry1.pack(pady='10', side='top')
+            self.button4 = ttk.Button(self.labelframe1)
+            self.button4.configure(text='Encode')
+            self.button4.pack(padx='100', pady='10', side='top')
+            self.button4.configure(command=self.encodebut)
+            self.separator2 = ttk.Separator(self.labelframe1)
+            self.separator2.configure(orient='horizontal')
+            self.separator2.pack(ipadx='150', pady='10', side='top')
+            self.label3 = ttk.Label(self.labelframe1)
+            self.label3.configure(text='Decode')
+            self.label3.pack(side='top')
+            self.entry3 = ttk.Entry(self.labelframe1)
+            self.decodetext = tk.StringVar(value='Encoded text here')
+            self.decodetext = tk.StringVar(value= "Encoded text here")
+            self.entry3.configure(textvariable=self.decodetext)
+            _text_ = '''Encoded text here'''
+            self.entry3.delete('0', 'end')
+            self.entry3.insert('0', _text_)
+            self.entry3.pack(pady='10', side='top')
+            self.button5 = ttk.Button(self.labelframe1)
+            self.button5.configure(text='Decode')
+            self.button5.pack(pady='10', side='top')
+            self.button5.configure(command=self.decodebut)
+            self.message1 = tk.Message(self.labelframe1)
+            self.output = tk.StringVar(value='')
+            self.message1.configure(textvariable=self.output)
+            self.message1.pack(pady='100', side='top')
+            self.labelframe1.configure(height='200', text='Numco', width='200')
+            self.labelframe1.pack(side='top')
+            self.labelframe1.bind('<1>', self.callback, add='')
+            self.frame1.configure(height='200', width='200')
+            self.frame1.pack(side='top')
+
+            # Main widget
+            self.mainwindow = self.frame1
     
-    elif encOrDec == "exit":
-        exit()
+        def encodebut(self):
+            output = str(self.entry1.get())
+            encoded = encode(output)
+            self.message1.configure(text=encoded)
+            self.message1.pack(pady='100', side='top')
+            print(encoded)
 
-function decode():
-    text = input("what do you want to decode? ")
+        def decodebut(self):
+            output = str(self.decodetext)
+            decoded = decode(output)
+            self.message1.configure(text=decoded)
+            self.message1.pack(pady='100', side='top')
+            print(decoded)
 
-    for letter in text:
-        decoded = decoded + inout[[f_inout[1] for f_inout in inout].index(letter)][0]
-        print(decoded)
+        def callback(self, event=None):
+            pass
+
+        def run(self):
+            self.mainwindow.mainloop()
+
+
+    if __name__ == '__main__':
+        root = tk.Tk()
+        app = NumcoGuiApp(root)
+        app.run()
+
+
+
+
+
+
+
+
+
+
+elif conorgui == "con":
+    while True:
+        encOrDec = input("Do you want to encode or decode (e/d/exit) ")
+        encoded = ''
+        decoded = ''
+        if encOrDec == "e":
+            text = input("What do you want to encode? ")
+            encoutput = encode(text)
+            print(encoutput)
+
+
+        elif encOrDec == "d":
+            text = input("What do you want to decode? ")
+            decoutput = decode(text)
+            print(decoutput)
+        elif encOrDec == "exit":
+            exit()
